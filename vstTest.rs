08f1b4bd423ee2dc99337ed00d6f49bb90831a36
Gage Howe <116420022+GageHoweTamu@users.noreply.github.com>
2023-11-13 23:59:06 -0600

    Add files via upload

---
+++ vstTest.rs
@@ -0,0 +1,47 @@
+#[macro_use]
+extern crate vst;
+extern crate rand;
+
+use vst::plugin::{Info, Plugin, Category};
+use vst::buffer::AudioBuffer;
+use rand::random;
+
+#[derive(Default)]
+struct Whisper;
+
+impl Plugin for Whisper {
+    fn get_info(&self) -> Info {
+        Info {
+            name: "Whisper".to_string(),
+
+            unique_id: 1337, 
+
+            inputs: 0,
+
+            outputs: 2,
+
+            category: Category::Synth,
+
+            ..Default::default()
+        }
+    }
+
+    fn process(&mut self, buffer: &mut AudioBuffer<f32>) {
+        
+        let (_, output_buffer) = buffer.split();
+
+        for output_channel in output_buffer.into_iter() {
+
+            for output_sample in output_channel {
+
+                *output_sample = (random::<f32>() - 0.5f32) * 2f32;
+            }
+        }
+    }
+}
+
+plugin_main!(Whisper); // need to add crate file
+
+// https://vaporsoft.net/creating-an-audio-plugin-with-rust-vst/
+
+// https://www.seventeencups.net/posts/writing-an-audio-plugin-in-rust/
